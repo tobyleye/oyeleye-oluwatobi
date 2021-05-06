829d5c3e8ebb82c228b98492d9524e17b528394a
Toby <31401879+tobyleye@users.noreply.github.com>
2021-05-06 11:45:01 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,83 @@
 import { useState } from "react";
 import "./styles.css";
 
-function Searchbar({ onSubmit }) {
-  const [query, setQuery] = useState("");
-
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit(query);
-        }}
-      >
-        <input
-          type="text"
-          required
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-    </div>
-  );
-}
-
 function search(arr1, query) {
-  const path = [];
-
-  function pathfinder(arr1, q, path = []) {
-    if (typeof arr1 === "object") {
-    }
-  }
+  // sarch function here;
+  return "result";
 }
 
 export default function App() {
   const [result, setResult] = useState("");
+  const [source, setSource] = useState(`
+  {
+    "user": {
+      "id": 1,
+      "name": {
+        "firstName": "James",
+        "lastName": "Ibori"
+      },
+      "location": {
+        "city": "Ikoyi",
+        "state": "Lagos",
+        "address": "One expensive house like that"
+      }
+    }
+  }
+  `);
+  const [query, setQuery] = useState("");
 
-  const search = (query) => {
-    console.log("searching for ...", query);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let parsedSource;
+    try {
+      parsedSource = JSON.parse(source);
+    } catch (err) {
+      alert("Enter a valid json object");
+      return;
+    }
+    const result = search(parsedSource, query);
+    setResult(result);
   };
 
   return (
     <div className="App">
       <h1>Path Getter</h1>
-      <Searchbar onSubmit={search} />
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="source">Source:</label>
+          <textarea
+            required
+            id="source"
+            value={source}
+            onChange={(e) => {
+              setSource(e.target.value);
+            }}
+            placeholder="source"
+          ></textarea>
+        </div>
+
+        <div className="field">
+          <label htmlFor="query">Query</label>
+          <input
+            id="query"
+            type="text"
+            required
+            value={query}
+            placeholder="query"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {result && (
+        <div className="result">
+          Result is
+          <br />
+          <span>{result}</span>
+        </div>
+      )}
     </div>
   );
 }
